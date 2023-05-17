f37fdda47c1e8870ddf95b6a9e567a85e6f78226
mohamed0yaser <90569015+mohamed0yaser@users.noreply.github.com>
2023-05-17 06:52:57 +0300

    Update views.py

--- api/views.py
+++ api/views.py
@@ -120,20 +120,23 @@ class ChangePasswordView(generics.UpdateAPIView):
 
 
 
-@permission_classes(AllowAny,)
-@api_view(['POST'])
-def embeddedCreate(request):
-   data = request.data
-   embedded = Embedded.objects.create(
-     temperature=data['temperature'],
-     humidity=data['humidity'],
-     light=data['light'],
-     rainfall=data['rainfall'],
-     soil_moisture=data['soil_moisture'],
-     pump_on=data['pump_on']
-   )
-   serializer = EmbeddedSerializer(embedded, many=False)
-   return Response(serializer.data)
+class EmbeddedCreate(APIView):
+  permission_classes = [permissions.AllowAny]
+  serializer_class = serializers.EmbeddedSerializer
+  def post(self,request):
+   serializer = serializers.EmbeddedSerializer(data=request.data)
+   if serializer.is_valid():
+     data = {
+                "temperature":serializer.data['temperature'],
+                "humidity":serializer.data['humidity'],
+                "light":serializer.data['light'],
+                "rainfall":serializer.data['rainfall'],
+                "soil_moisture":serializer.data['soil_moisture'],
+                "pump_on":serializer.data['pump_on']
+     }
+     return Response(data)
+   else:
+       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 def embeddedViews(request):
